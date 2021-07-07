a7a3b9517f77c0e5797c3203e6b224db8f3b1564
neggplant <cq4050@163.com>
2021-07-07 15:36:34 +0800

    序列化 > 使用序列化类

--- tutorial_drf/settings.py
+++ tutorial_drf/settings.py
@@ -35,8 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quickstart',
+    'quickstart.apps.QuickstartConfig',
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
