87ce14c1903fc97cead301d101d92c0056eb5c58
neggplant <cq4050@163.com>
2021-07-07 15:50:59 +0800

    序列化 > 使用ModelSerilializers

--- tutorial_drf/urls.py
+++ tutorial_drf/urls.py
@@ -10,5 +10,6 @@ router.register(r'groups', views.GroupViewSet)
 # 另外，我们还包括支持浏览器浏览API的登录URL。
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
