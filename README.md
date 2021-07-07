b37f692d1c763de9a11695d3f373da0edacb119b
neggplant <cq4050@163.com>
2021-07-07 16:06:18 +0800

    请求和响应 > 使用装饰器实现视图函数，实现.json可以访问

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # tutorial_drf
+
+## 请求与相应
+###请求
+```bash
+request.POST  # 只处理表单数据  只适用于'POST'方法
+request.data  # 处理任意数据  适用于'POST'，'PUT'和'PATCH'方法
+```
+### 响应
+```bash
+return Response(data)  # 渲染成客户端请求的内容类型。
+```
+### 包装（wrapping）API视图
+REST框架提供了两个可用于编写API视图的包装器（wrappers）。
+
+用于基于函数视图的@api_view装饰器。  
+用于基于类视图的APIView类。
--- tutorial_drf/urls.py
+++ tutorial_drf/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from quickstart import views
 
 router = routers.DefaultRouter()
@@ -9,7 +11,9 @@ router.register(r'groups', views.GroupViewSet)
 # 使用自动URL路由连接我们的API。
 # 另外，我们还包括支持浏览器浏览API的登录URL。
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    # url(r'^', include(router.urls)),
     url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
